43927de1b8de0dbac282d9d9865785a8101686b6
moriakaplan <mokaplan@g.jct.ac.il>
2022-02-22 18:53:51 +0200

    implement crossProduct function

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -3,8 +3,7 @@ package primitives;
 public class Vector extends Point {
     public Vector(double x, double y, double z) {
         super(x, y, z);
-        if (xyz.equals(Double3.ZERO))
-        {
+        if (xyz.equals(Double3.ZERO)) {
             throw new IllegalArgumentException("zero vector is not allowed");
         }
     }
@@ -14,41 +13,42 @@ public class Vector extends Point {
         return "Vector{}";
     }
 
-    public Vector(Double3 d)
-    {
+    public Vector(Double3 d) {
         this(d.d1, d.d2, d.d3);
     }
-    public Vector subtract(Vector other)
-    {
-        return super.subtract((Point)other); /*#*/
+
+    public Vector subtract(Vector other) {
+        return super.subtract((Point) other); /*#*/
     }
-    public Vector add(Vector vec)
-    {
-        return (Vector)super.add(vec); /*#*/
+
+    public Vector add(Vector vec) {
+        return (Vector) super.add(vec); /*#*/
     }
-    public Vector scale(double num)
-    {
+
+    public Vector scale(double num) {
         return new Vector(xyz.scale(num));
     }
-    public double dotProduct(Vector vec)
-    {
+
+    public double dotProduct(Vector vec) {
         Double3 d = xyz.product(vec.xyz);
         return d.d1 + d.d2 + d.d3;
     }
-    public Vector crossProduct(Vector vec)
-    {
-        return new Vector(1, 1, 1); /*#*/
+
+    public Vector crossProduct(Vector vec) {
+        Double3 a = this.xyz;
+        Double3 b = vec.xyz;
+        return new Vector(a.d2 * b.d3 - a.d3 * b.d2, a.d3 * b.d1 - a.d1 * b.d3, a.d1 * b.d2 - a.d2 * b.d1);
     }
-    public double lengthSquared()
-    {
+
+    public double lengthSquared() {
         return this.dotProduct(this);
     }
-    public double length()
-    {
+
+    public double length() {
         return Math.sqrt(this.lengthSquared());
     }
-    public Vector normalize()
-    {
+
+    public Vector normalize() {
         return this.scale(1 / this.length());
     }
 }
