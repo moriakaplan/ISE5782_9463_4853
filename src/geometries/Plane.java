f6a3d1b5ba321b99f659404a759bbd8adec1d833
moriakaplan <mokaplan@g.jct.ac.il>
2022-03-07 12:09:15 +0200

    notes for Plane and Tube.

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -3,23 +3,46 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * This class represents a plane in three-dimensional space by point on the plane and vertical vector.
+ */
 public class Plane implements Geometry {
     private Point q0;
     private Vector normal;
 
+    /**
+     * Constructor with point on the plane and normal vector.
+     * @param q0 point on the plane
+     * @param normal normal vector
+     */
     public Plane(Point q0, Vector normal) {
         this.q0 = q0;
         this.normal = normal.normalize();
     }
+
+    /**
+     * Constructor with 3 points on the plane (select one point for the field q0 and find a normal vector to those points).
+     * @param p1 first point.
+     * @param p2 second point.
+     * @param p3 third point.
+     */
     public Plane(Point p1, Point p2, Point p3) {
         this.q0 = p1;
         this.normal = null;
     }
 
+    /**
+     * getter for field q0
+     * @return q0- the point on the plane.
+     */
     public Point getQ0() {
         return q0;
     }
 
+    /**
+     * getter for field normal
+     * @return the vector that is normal to the plane.
+     */
     public Vector getNormal() {
         return normal;
     }
--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,10 +1,18 @@
 package geometries;
 import primitives.*;
 
+/**
+ * This class represents an infinity Tube in three-dimensional space by the center axis of the tube (ray) and its radius.
+ */
 public class Tube implements Geometry{
     protected Ray axisRay;
     protected double radius;
 
+    /**
+     * Constructor, using the values for the fields.
+     * @param axisRay the center axis of the tube.
+     * @param radius the length of the radius of the tube.
+     */
     public Tube(Ray axisRay, double radius) {
         if (radius <= 0)
         {
@@ -14,10 +22,18 @@ public class Tube implements Geometry{
         this.radius = radius;
     }
 
+    /**
+     * getter for field AxisRay.
+     * @return the center axis of the tube.
+     */
     public Ray getAxisRay() {
         return axisRay;
     }
 
+    /**
+     * Getter for field radius.
+     * @return the length of the radius of the tube.
+     */
     public double getRadius() {
         return radius;
     }
