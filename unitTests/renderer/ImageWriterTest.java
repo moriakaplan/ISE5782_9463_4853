b9cbb6c6ac206f57a854964cb4532a9a0dbb7181
moriakaplan <mokaplan@g.jct.ac.il>
2022-04-05 12:57:27 +0300

    step 1- testing class ImageWriter- create empty grid.

---
+++ unitTests/renderer/ImageWriterTest.java
@@ -0,0 +1,25 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTest {
+
+    @Test
+    void testWritePixel() {
+        int nX = 800, nY = 500, gap = 50;
+        Color green = new Color(0, 150, 120);
+        ImageWriter writer = new ImageWriter("grid", nX, nY);
+        for (int i = 0; i < nY; i++) {
+            for (int j = 0; j < nX; j++) {
+                if (i % gap == 0 || j % gap == 0)
+                    writer.writePixel(j, i, Color.BLACK);
+                else
+                    writer.writePixel(j, i, green);
+            }
+        }
+        writer.writeToImage();
+    }
+}
